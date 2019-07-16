16790996ba373826e60d7209cdb2195a84073429
david <davidjohansson35@gmail.com>
2019-07-16 21:06:36 +0200

    Added get_screen_size and get_screen_geometry

--- tk_tools.py
+++ tk_tools.py
@@ -1,3 +1,15 @@
+from DTools.screeninfo import get_monitors
+
+
+def get_screen_geometry(index):
+    m = get_monitors()[index]
+    return '{}x{}+{}+{}'.format(m.width, m.height, m.x, m.y)
+
+
+def get_screen_size(index):
+    m = get_monitors()[index]
+    return m.width, m.height
+
 
 def color_to_tk(color):
     return '#{}{}{}'.format(*["{0:#0{1}x}".format(el, 4).replace("0x", "") for el in color])
